026134de80cdad929bf1e922f374e64d2de24873
Godwin Odenigbo <godwin.odenigbo@sky.uk>
2024-07-02 17:11:38 +0100

    fix: updated createPayment path

--- src/handlers/payments.ts
+++ src/handlers/payments.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import { createPayment } from '../handlers/payments/create-payment';
+import { createPayment } from '../core/payments/create-payment';
 
 export const handler = async (event: APIGatewayProxyEvent) => {
     return await createPayment(event.body);  
